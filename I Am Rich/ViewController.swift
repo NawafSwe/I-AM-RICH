bafab9d99f9a14ede428cf0cc5e8ca0c137ec30d
Nawaf <nawafbader@MacBook-Pro.local>
2020-06-26 19:30:44 +0300

    first ios app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nawaf B Al sharqi on 05/11/1441 AH.
+//  Copyright © 1441 Nawaf B Al sharqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
